77d5945c3213099cc8208e3ac79e35c77017c4fd
K-Othman <karim_othman88@hotmail.com>
2023-04-26 08:48:19 +0100

    created the landing page

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,32 @@
+import landing3 from "../imgs/landing3.jpg";
+import { Link } from "react-router-dom";
+
+export default function LandingPage() {
+  return (
+    <section>
+      <div>
+        <div className="relative">
+          <div>
+            <img
+              className="h-[calc(100vh-68px)] min-w-full"
+              src={landing3}
+              alt="landing foto for the hotel"
+            />
+          </div>
+          <div className="text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white bg-[rgb(0,0,0,0.5)] py-12 w-full md:max-w-[550px] md:mx-auto">
+            <p className="text-5xl tracking-wider mb-5">Luxurious Rooms</p>
+            <p className="text-xl tracking-widest mb-6">
+              Deluxe Rooms Starting At £399
+            </p>
+            <Link
+              className="bg-secondary-color py-2 px-3 tracking-wider"
+              to="/rooms"
+            >
+              OUR ROOMS
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,9 @@
+import LandingPage from "../components/LandingPage";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <section>
+      <LandingPage />
+    </section>
+  );
 }
