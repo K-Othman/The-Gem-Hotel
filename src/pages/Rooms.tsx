c71fdba57b80d7e706f77d52de5f763ff0a02c65
K-Othman <karim_othman88@hotmail.com>
2023-04-28 00:43:01 +0100

    created sorting by price, allowed and not allowed funcs

--- src/pages/Rooms.tsx
+++ src/pages/Rooms.tsx
@@ -1,9 +1,11 @@
 import RoomsBanner from "../components/RoomsBanner";
+import SearchRooms from "../components/SearchRooms";
 
 export default function Rooms() {
   return (
     <section>
       <RoomsBanner />
+      <SearchRooms />
     </section>
   );
 }
