3024eb5c6c9b7954f09d18b53e6a462b5676293c
K-Othman <karim_othman88@hotmail.com>
2023-05-15 23:59:54 +0100

    fixed the footer

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import FeaturedRooms from "../../components/FeaturedRooms/FeaturedRooms";
-import Footer from "../../components/Footer/Footer";
+
 import LandingPage from "../../components/LandingPage/LandingPage";
 import Services from "../../components/Services/Services";
 
@@ -9,7 +9,6 @@ export default function Home() {
       <LandingPage />
       <Services />
       <FeaturedRooms />
-      <Footer />
     </section>
   );
 }
